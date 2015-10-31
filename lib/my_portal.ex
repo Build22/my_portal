d37623dde5d2ef47a1a514eb768de9769cd45608
Nagaraja Jade <hello@build22.org>
2015-10-31 16:20:23 +0000

    setup base logic

--- lib/my_portal.ex
+++ lib/my_portal.ex
@@ -16,4 +16,33 @@ defmodule MyPortal do
     opts = [strategy: :one_for_one, name: MyPortal.Supervisor]
     Supervisor.start_link(children, opts)
   end
+
+  defstruct [:left, :right]
+
+  @doc """
+  Starts transfering `data` from `left` to `right`
+  """
+  def transfer(left, right, data) do
+    # First add all data to the portal on the left
+    for item <- data do
+      Portal.Door.push(left, item)
+    end
+    # Returns a Portal struct we will use next
+    %MyPortal{left: left, right: right}
+  end
+
+  @doc """
+  Pushes data to the right into the given portal
+  """
+  def push_right(portal) do
+  # See if we can pop data from left. If so, push the
+  # popped data to the right. Otherwise, do nothing.
+    case Portal.Door.pop(portal.left) do
+      :error -> :ok
+      {:ok, h} -> Portal.Door.push(portal.right, h)
+    end
+  # Let's return the portal itself
+  portal
+  end
+
 end
